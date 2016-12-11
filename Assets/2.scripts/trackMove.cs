3fc52150834804a5bf7f1ebaf6daf15721287bf6
Chih-Yung Liang <r05944012@csie.ntu.edu.tw>
2016-12-12 01:33:21 +0800

    Fix that not clamp elev angle & add deltatime for reality

--- Assets/2.scripts/trackMove.cs
+++ Assets/2.scripts/trackMove.cs
@@ -3,30 +3,20 @@ using System.Collections;
 
 public class trackMove : MonoBehaviour {
 
-	public float mSpeed = 1;
-	public float rSpeed = 1;
+	public float mSpeed = 10;
+	public float rSpeed = 10;
+	public GameObject barrel;
 
-	float towerSpeed = 1;
-
-	public GameObject barrel;//砲管
-	float barrelSpeed = 30;
-	float maxAngle = 80;
-	float minAngle = -5;
-	float angle;
-
-	// Use this for initialization
 	void Start () {
-	
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 		Vector3 towerRot = transform.localEulerAngles;
-		towerRot.z += ControlScript.rotation;
+		towerRot.z += ControlScript.rotation * rSpeed * Time.deltaTime;
 		transform.localEulerAngles = towerRot;
 
 		Vector3 barrelEle = barrel.transform.localEulerAngles;
-		barrelEle.x += ControlScript.elevation;
+		barrelEle.x = Mathf.Clamp(barrelEle.x + ControlScript.elevation * mSpeed * Time.deltaTime, 0, 80);
 		barrel.transform.localEulerAngles = barrelEle;
 	}
 }
