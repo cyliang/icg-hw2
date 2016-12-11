4c1c9c25f04a51a492a1ec85732808caea38c033
Chih-Yung Liang <r05944012@csie.ntu.edu.tw>
2016-12-12 01:15:00 +0800

    Make bullet explode on timeout.

---
+++ Assets/2.scripts/BulletBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class BulletBehaviour : MonoBehaviour {
+	public GameObject effect;
+	public string[] targetColliders;
+	public float timeToExplode = 5;
+
+	float showTime;
+
+	void Start () {
+		showTime = Time.time;	
+	}	
+
+	void Update () {
+		if (Time.time - showTime > 5)
+			explode ();
+	}
+
+	void OnCollisionEnter (Collision collision) {
+		if(Array.IndexOf(targetColliders, collision.gameObject.tag) != -1){
+			explode ();
+		}	
+	}
+
+	void explode() {
+		Instantiate (effect, transform.position, transform.rotation);
+		Destroy(gameObject);
+	}
+}
